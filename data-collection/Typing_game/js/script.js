535be2f8c6863788c578e99e06585ef29b3acd16
Xyl0w <xaviertung@hotmail.sg>
2022-09-09 11:57:29 +0800

    Added lap and timer to track intervals

--- data-collection/Typing_game/js/script.js
+++ data-collection/Typing_game/js/script.js
@@ -5,7 +5,7 @@ timeTag = document.querySelector(".time span b"),
 mistakeTag = document.querySelector(".mistake span"),
 wpmTag = document.querySelector(".wpm span"),
 cpmTag = document.querySelector(".cpm span");
-
+window.onload = window.localStorage.clear();
 let timer,
 maxTime = 60,
 timeLeft = maxTime,
@@ -90,6 +90,7 @@ loadParagraph();
 inpField.addEventListener("input", initTyping);
 tryAgainBtn.addEventListener("click", resetGame);
 
+
 function duration(timestamps) {
     var last = timestamps.pop();
     var durations = [];
@@ -104,19 +105,259 @@ function duration(timestamps) {
         return (mills / 1000) + ' s';
     return mills + ' ms';
   }
+  // Not perfect, but quickest to understand and build upon:
+// Original https://www.sitepoint.com/community/t/javascript-how-to-make-laps-in-the-stopwatch/244936/2
+
+
+    var hours = 00;
+    var minutes = 00;
+    var seconds = 00;
+    var tens = 00;
+    var aHours = document.getElementById("hours");
+    var aMinutes = document.getElementById("minutes");
+    var aSeconds = document.getElementById("seconds");
+    var aTens = document.getElementById("tens");
+    var Start = document.getElementById("start");
+    var Stop = document.getElementById("stop");
+    var reset = document.getElementById("reset");
+    var clear = document.getElementById("clear");
+    var Interval;
+    var Lap = document.getElementById("lap");
+    var Laps = document.getElementById("laps");
+    var lapCount = 1;
+    var lastLap = { hours: 0, minutes: 0, tens: 0, seconds: 0 };
   
+    function leftPad(value) {
+      return value < 10 ? "0" + value : value;
+    }
+    
+    // localStorage detection
+    function supportsLocalStorage() {
+      return typeof(Storage)!== 'undefined';
+    }
+    
+    $('#stop').hide();
+  
+    Start.onclick = function() {
+      clearInterval(Interval);
+      Interval = setInterval(startTimer, 10);
+      $('#stop').toggle();
+      $('#start').toggle();
+    };
+  
+    Stop.onclick = function() {
+      clearInterval(Interval);
+      $('#start').toggle();
+      $('#stop').toggle();
+    };
+  
+    reset.onclick = function() {
+      clearInterval(Interval);
+      hours = "00";
+      minutes = "00";
+      seconds = "00";
+      tens = "00";
+      aHours.innerHTML = hours;
+      aMinutes.innerHTML = minutes;
+      aSeconds.innerHTML = seconds;
+      aTens.innerHTML = tens;
+    };
+
+    
+    function startTimer() {
+      
+      tens++;
+  
+      if (tens < 9) {
+        aTens.innerHTML = "0" + tens;
+      }
+  
+      if (tens > 9) {
+        aTens.innerHTML = tens;
+      }
+  
+      if (tens > 99) {
+        // console.log("seconds");
+        seconds++;
+        aSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        aTens.innerHTML = "0" + 0;
+      }
+  
+      if (seconds > 9) {
+        aSeconds.innerHTML = seconds;
+      }
+  
+      if (seconds > 59) {
+        // console.log("minutes");
+        minutes++;
+        aMinutes.innerHTML = "0" + minutes;
+        seconds = 0;
+        aSeconds.innerHTML = "0" + 0;
+        tens = 0;
+        aTens.innerHTML = "0" + 0;
+      }
+  
+      if (minutes > 9) {
+        aMinutes.innerHTML = minutes;
+      }
+  
+      if (minutes > 59) {
+        // console.log("seconds");
+        hours++;
+        aHours.innerHTML = "0" + hours;
+        minutes = 0;
+        aMinutes.innerHTML = "0" + 0;
+        seconds = 0;
+        aSeconds.innerHTML = "0" + 0;
+        tens = 0;
+        aTens.innerHTML = "0" + 0;
+      }
+  
+      if (hours > 9) {
+        aHours.innerHTML = hours;
+      }
+    }
+    
+    
+  
+    // Run the support check
+    if (!supportsLocalStorage()) {
+      console.log('browser storage not supported')
+      
+      Lap.onclick = function() {
+        var lapHours = hours - lastLap.hours;
+        var lapMinutes = minutes - lastLap.minutes;
+        if (lapMinutes < 0) {
+          var lapMinutes = minutes - lastLap.minutes + 60;
+        }
+        var lapSeconds = seconds - lastLap.seconds;
+        if (lapSeconds < 0) {
+          var lapSeconds = seconds - lastLap.seconds + 60;
+        }
+        var lapTens = tens - lastLap.tens;
+        if (lapTens < 0) {
+          var lapTens = tens - lastLap.tens + 100;
+        }
+        lastLap = {
+          tens: tens,
+          seconds: seconds,
+          minutes: minutes,
+          hours: hours
+        };
+  
+        Laps.innerHTML +=
+          "<li>" +
+          leftPad(lapHours) +
+          ":" +
+          leftPad(lapMinutes) +
+          ":" +
+          leftPad(lapSeconds) +
+          ":" +
+          leftPad(lapTens) +
+          "</li>";
+      };
+      // Just clear laps list
+      clear.onclick = function() {
+        Laps.innerHTML = '';
+      }
+    } else {
+  
+      // HTML5 localStorage Support
+      try {
+        Lap.onclick = function() {
+          var lapHours = hours - lastLap.hours;
+          var lapMinutes = minutes - lastLap.minutes;
+          if (lapMinutes < 0) {
+            var lapMinutes = minutes - lastLap.minutes + 60;
+          }
+          var lapSeconds = seconds - lastLap.seconds;
+          if (lapSeconds < 0) {
+            var lapSeconds = seconds - lastLap.seconds + 60;
+          }
+          var lapTens = tens - lastLap.tens;
+          if (lapTens < 0) {
+            var lapTens = tens - lastLap.tens + 100;
+          }
+          lastLap = {
+            tens: tens,
+            seconds: seconds,
+            minutes: minutes,
+            hours: hours
+          };
+  
+          Laps.innerHTML +=
+            "<li>" +
+            "Lap " + lapCount++ + " – " +
+            leftPad(lapHours) +
+            ":" +
+            leftPad(lapMinutes) +
+            ":" +
+            leftPad(lapSeconds) +
+            "." +
+            leftPad(lapTens) +
+            "</li>";
+          
+          localStorage.setItem('laps', JSON.stringify(Laps.innerHTML));
+          // console.log(localStorage.getItem('laps'));
+        };
+  
+        clear.onclick = function() {
+          Laps.innerHTML = '';
+          localStorage.removeItem('laps');
+        }
+      }
+      
+      catch (e) {
+      
+        // If any errors, catch and alert the user
+        if (e == QUOTA_EXCEEDED_ERR) {
+          alert('Quota exceeded!');
+        }
+      }
+    
+      if (localStorage.getItem('laps')) {
+      
+        // Retrieve the item
+        var storedLaps = JSON.parse(localStorage.getItem('laps'));
+        $('#laps').html(storedLaps);
+        // console.log(localStorage.getItem('laps'));
+      }
+    }
+
+
   var durations = [];
-  
+  var timestamps = [];
+  var words = [];
+  //upon keypress start timer
+  $(document).keypress(function(e) { Start.click(); });
+  $(document).keypress(function(e) { Lap.click(); });
   $('#in').keydown(function (e) {
+    //Second iteration keypress event
+    
     durations.push($.now());
   }).keyup(function (e) {
     var current = durations;
     current.push($.now());
     durations = [];
     var timeElapsed = current[current.length - 1] - current[0];
-  
+
+    // find the interval between each press, current time - last key press time
+    var time_interval = (duration(current)) 
+
+    timestamps.push(time_interval);
+    words.push(e.keyCode)
+    console.log(time_interval)
     console.log([
-        ['time elapsed:', display(timeElapsed)].join(' '),
-        ['keys duration:', duration(current).map(display)].join(' ')
+        ['Key code:', e.keyCode].join(' '),
+        ['Key hold length:', display(timeElapsed)].join(' '),
+        ['Interval between each key press:', ].join(' ')
     ].join(' --- '));
+    
   });
+
+  function lap() {
+    lapNow = `<div class="lap">${hours} : ${minutes} : ${seconds} : ${miliseconds}</div>`;
+    words.innerHTML += lapNow;
+  }
+  
